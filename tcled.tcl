4755cb213acce51d0bc5a468aede09489635e1a4
dislux-hapfyl <149989981+dislux-hapfyl@users.noreply.github.com>
2026-08-10 14:05:34 -0700

    Update tcled.tcl

--- tcled.tcl
+++ tcled.tcl
@@ -58,6 +58,71 @@ proc Tclme::BuildUI {} {
     Tclme::ApplyTheme
 }
 
+
+# ----------------------------------------------------------------------------
+# Medium Solarized
+# ----------------------------------------------------------------------------
+
+proc Tclme::SolarizedMedium {} {
+    # Solarized palette used:
+    #
+    # base03   #002b36
+    # base02   #073642
+    # base01   #586e75
+    # base00   #657b83
+    # base0    #839496
+    # base1    #93a1a1
+    # base2    #eee8d5
+    # base3    #fdf6e3
+    # yellow   #b58900
+    # orange   #cb4b16
+    # red      #dc322f
+    # magenta  #d33682
+    # violet   #6c71c4
+    # blue     #268bd2
+    # cyan     #2aa198
+    # green    #859900
+
+    return [dict create \
+        bg          "#eee8d5" \
+        fg          "#657b83" \
+        editor_bg   "#eee8d5" \
+        editor_fg   "#586e75" \
+        status_bg   "#073642" \
+        status_fg   "#93a1a1" \
+        minibuf_bg  "#eee8d5" \
+        minibuf_fg  "#586e75" \
+        accent      "#268bd2" \
+        separator   "#d8d1be" \
+        scrollbar   "#cfc7b2" \
+        cursor      "#cb4b16" \
+        font        {Consolas 12} \
+        status_font {Consolas 10} \
+    ]
+
+}
+
+
+proc Tclme::SolarizedMediumDark {} {
+    return [dict create \
+        bg          "#002b36" \
+        fg          "#839496" \
+        editor_bg   "#073642" \
+        editor_fg   "#93a1a1" \
+        status_bg   "#002b36" \
+        status_fg   "#93a1a1" \
+        minibuf_bg  "#073642" \
+        minibuf_fg  "#93a1a1" \
+        accent      "#268bd2" \
+        separator   "#0e4c58" \
+        scrollbar   "#0e4c58" \
+        cursor      "#cb4b16" \
+        font        {Consolas 12} \
+        status_font {Consolas 10} \
+    ]
+
+}
+
 proc Tclme::LightPalette {} {
     return [dict create \
         bg          "#F5F5F5" \
@@ -122,6 +187,10 @@ proc Tclme::UseTheme {name} {
         set theme [dict merge $theme [Tclme::DarkPalette]]
     } elseif {$name eq "light"} {
         set theme [dict merge $theme [Tclme::LightPalette]]
+    } elseif {$name eq "sol"} {
+        set theme [dict merge $theme [Tclme::SolarizedMedium]]
+    } elseif {$name eq "sold"} {
+        set theme [dict merge $theme [Tclme::SolarizedMediumDark]]
     } else {
         Tclme::Message "Unknown theme: $name"
         return
@@ -1396,15 +1465,9 @@ proc Tclme::CmdGoto {args} {
 }
 
 proc Tclme::CmdTheme {args} {
-    set arg [string trim [join $args " "]]
+    set name [string trim [join $args " "]]
 
-    if {$arg eq ""} {
-        Tclme::Message "Theme: use :theme light or :theme dark"
-    } elseif {$arg eq "light" || $arg eq "dark"} {
-        Tclme::UseTheme $arg
-    } else {
-        Tclme::Message "Unknown theme: $arg"
-    }
+    Tclme::UseTheme $name
 }
 
 proc Tclme::SwitchByNameOrIndex {target} {
@@ -1464,7 +1527,7 @@ proc Tclme::InitGUI {} {
     Tclme::DefCommand reload-init Tclme::CmdReloadInit  "Reload init file"
     Tclme::DefCommand theme       Tclme::CmdTheme       "Switch theme: light/dark"
     Tclme::DefCommand scratch     Tclme::CmdScratch     "Switch to scratch or create new"
-    
+
     Tclme::DefAlias rp  repl
     Tclme::DefAlias n   new
     Tclme::DefAlias sc  scratch
